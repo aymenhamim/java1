5057db2749be06873c98c1eda32fa6ad82657f61
aymen hamim <aymenhamim2004@gmail.com>
2025-08-31 14:10:33 +0100

    Linked List

---
+++ src/linkedlist/Main.java
@@ -0,0 +1,31 @@
+package linkedlist;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        LinkedList LL1 = new LinkedList(1);
+        LL1.append(2);
+        LL1.append(3);
+        LL1.append(4);
+        LL1.append(5);
+
+        LL1.prepend(0);
+
+        LL1.set(6, 33);
+
+//        LL1.printList();
+//        System.out.println( "\n" + LL1.findMiddleNode());
+//        System.out.println( "\n" + LL1.hasLoop());
+
+        // Linked list Leetcode challenge
+
+        List LL2 = new java.util.LinkedList<>();
+
+
+
+
+
+
+    }
+}
