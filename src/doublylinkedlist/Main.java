ef105a8d93f509291ba444068a426c9c8b7cbe1c
aymenhamim <aymenhamim2004@gmail.com>
2025-09-01 10:32:31 +0100

    Implement get method in DoublyLinked list class

--- src/doublylinkedlist/Main.java
+++ src/doublylinkedlist/Main.java
@@ -2,20 +2,22 @@ package doublylinkedlist;
 
 public class Main {
     public static void main(String[] main){
-        DoublyLinkedList LL1 = new DoublyLinkedList(1);
+        DoublyLinkedList DLL1 = new DoublyLinkedList(1);
 
-        LL1.append(2);
-        LL1.append(3);
-        LL1.prepend(0);
+        DLL1.append(2);
+        DLL1.append(3);
+        DLL1.prepend(0);
 
-//        LL1.prepend(-1);
-//        LL1.removeFirst();
+//        DLL1.prepend(-1);
+//        DLL1.removeFirst();
 
-//        System.out.println(LL1.removeLast().value);
+//        System.out.println(DLL1.removeLast().value);
 
-        LL1.getHead();
-        LL1.getTail();
-        LL1.getLength();
+//        DLL1.getHead();
+//        DLL1.getTail();
+//        DLL1.getLength();
+
+        System.out.println(DLL1.get(5).value);
 
     }
 }
