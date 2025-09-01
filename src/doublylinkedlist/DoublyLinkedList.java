d83de566022fa2caf94b4f69fa5f284edc13495a
aymenhamim <aymenhamim2004@gmail.com>
2025-09-01 08:21:25 +0100

    Implement append method in DoublyLinked list class

--- src/doublylinkedlist/DoublyLinkedList.java
+++ src/doublylinkedlist/DoublyLinkedList.java
@@ -10,7 +10,7 @@ public class DoublyLinkedList {
     public class Node{
         int value;
         Node next;
-        Node previous;
+        Node prev;
 
         public Node(int value){
             this.value = value;
@@ -25,7 +25,22 @@ public class DoublyLinkedList {
     }
 
     public void getHead(){System.out.println("Head: " + head.value + "\n");}
-    public void getTail(){System.out.println("Tail: " + head.value + "\n");}
+    public void getTail(){System.out.println("Tail: " + tail.value + "\n");}
     public void getLength(){System.out.println("Length: " + length + "\n");}
+
+    public void append(int value){
+        Node newNode = new Node(value);
+
+        if(length == 0){
+            head = newNode;
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+
+        length ++;
+    }
 
 }
--- src/doublylinkedlist/Main.java
+++ src/doublylinkedlist/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] main){
         DoublyLinkedList LL1 = new DoublyLinkedList(1);
 
+        LL1.append(2);
+
         LL1.getHead();
         LL1.getTail();
         LL1.getLength();
+
     }
 }
