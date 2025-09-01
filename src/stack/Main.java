764d3716b74ce6b71108eb7a00f903922bae06d0
aymen hamim <aymenhamim2004@gmail.com>
2025-09-01 17:43:39 +0100

    Implement Stack Pop()

--- src/stack/Main.java
+++ src/stack/Main.java
@@ -2,10 +2,13 @@ package stack;
 
 public class Main {
     public static void main(String[] args){
-        Stack Stack1 = new Stack(5);
+        Stack Stack1 = new Stack(3);
 
-        Stack1.push(4);
-        Stack1.push(3);
+        Stack1.push(2);
+        Stack1.push(1);
+        Stack1.push(0);
+
+        Stack1.pop();
 
         Stack1.getTop();
         Stack1.getHeight();
