01ee0c6c177022b46076930a9f28eae00c825815
aymen hamim <aymenhamim2004@gmail.com>
2025-09-02 19:54:23 +0100

    Implement BST Insert method

--- src/binarysearchtree/Main.java
+++ src/binarysearchtree/Main.java
@@ -2,10 +2,17 @@ package binarysearchtree;
 
 public class Main {
     public static void main(String[] args) {
-        BinarySearchTree myBinarySearchTree = new BinarySearchTree();
+        BinarySearchTree myBST1 = new BinarySearchTree();
 
-        System.out.println("root = " + myBinarySearchTree.root);
+        myBST1.insert(47);
+        myBST1.insert(20);
+        myBST1.insert(60);
+        myBST1.insert(27);
+        myBST1.insert(15);
+
+        myBST1.insert(81);
 
 
+        System.out.println("root = " + myBST1.root.left.right.value);
     }
 }
