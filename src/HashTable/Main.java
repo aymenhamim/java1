9d885fba5e61aa4ec3ae24913e0c372c78f7a589
aymen hamim <aymenhamim2004@gmail.com>
2025-09-02 20:41:35 +0100

    Implement HashTable Set  Method

--- src/HashTable/Main.java
+++ src/HashTable/Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         HashTable myHT = new HashTable();
 
+        myHT.set("age", 21);
+        myHT.set("nails", 100);
+        myHT.set("tile", 80);
+
+        myHT.set("bolts", 150);
+
         myHT.printTable();
     }
 }
